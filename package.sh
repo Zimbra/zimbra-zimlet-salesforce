8b3e24007bce0fb09e3975865887fff3a31d78c3
Barry de Graaff <4353213+barrydegraaff@users.noreply.github.com>
2023-02-06 12:27:38 +0100

    update dependencies

--- package.sh
+++ package.sh
@@ -2,4 +2,4 @@
 
 npm install
 zimlet build
-zimlet package -v 1.0.5 --zimbraXVersion ">=2.0.0" -n "zimbra-zimlet-salesforce" --desc "Adds a button in the more menu to create a Salesforce case from an email." -l "Salesforce Email2Case"
+zimlet package -v 1.0.6 --zimbraXVersion ">=2.0.0" -n "zimbra-zimlet-salesforce" --desc "Adds a button in the more menu to create a Salesforce case from an email." -l "Salesforce Email2Case"
